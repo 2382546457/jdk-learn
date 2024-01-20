dc45b6f3d707d07bded3ed173abb003fa70618dc
2382546457 <2382546457@qq.com>
2024-01-20 15:12:14 +0800

    ForkJoinPool项目创建

---
+++ ForkJoinPool/src/main/java/org/xiaohe/Main.java
@@ -0,0 +1,12 @@
+package org.xiaohe;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2024-01-20 15:11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- Timer/src/main/java/org/xiaohe/Main.java
+++ Timer/src/main/java/org/xiaohe/Main.java
@@ -1,12 +1,27 @@
 package org.xiaohe;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @author : 小何
  * @Description :
  * @date : 2024-01-18 13:53
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(() -> {
+                System.out.println("开始");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+
     }
 }
