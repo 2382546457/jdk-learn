4193e222759698ad709ddab06633b8aba0b635a6
2382546457 <2382546457@qq.com>
2024-02-10 21:43:25 +0800

    对象池创建

---
+++ ObjectPool/src/main/java/com/xiaohe/Main.java
@@ -0,0 +1,12 @@
+package com.xiaohe;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2024-02-10 21:42
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
